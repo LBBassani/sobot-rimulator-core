705fbb1c023c798f2b7670d623091aa01b25a111
LBBassani <lorenabassani12@gmail.com>
2020-06-04 11:05:37 -0300

    nova versão

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='srimulatorcore',  
-     version='0.3.2',
+     version='0.3.3',
      author="Lorena B Bassani",
      author_email="lorenabassani12@gmail.com",
      description="Core of Sobot Rimulator by Nick McCrea",
